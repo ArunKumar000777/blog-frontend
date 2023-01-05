6258a7b003e7bf3ecf531e4d433eb90bcccd4449
ArunKumar000777 <arunashok1020@gmail.com>
2023-01-05 15:51:22 +0530

    change the base url2

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://blog-app-api-b3n3.onrender.com";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
