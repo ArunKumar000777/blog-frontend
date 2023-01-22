2d89d34098788c0a7c932db7171eedb80931b3cf
ArunKumar000777 <arunashok1020@gmail.com>
2023-01-22 05:41:08 +0530

    change req url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const BASE_URL = "https://blog-app-api-b3n3.onrender.com/api";
+const BASE_URL = "https://blog-app-api-b3n3.onrender.com/api";
 
 // export const publicRequest = axios.create({
 //     baseURL: BASE_URL,
 // });
-const BASE_URL = "http://localhost:5000/api/";
+// const BASE_URL = "http://localhost:5000/api/";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
